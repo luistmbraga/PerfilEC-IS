d801a211d622e53f138be29828c175a8712e0f89
Luís Braga <a82088@alunos.uminho.pt>
2020-04-02 02:51:20 +0100

    data analyser

---
+++ FE02/DataAnalyser/dataAnalyser.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import mysql.connector
+con = mysql.connector.connect(user='root', password='admin', host='127.0.0.1', database='fe02MaquinaA')
+
+query = "SELECT mensagemTxt, roundTripTime FROM fe02maquinaa.mensagem order by LENGTH(mensagemTxt) ASC, roundTripTime ASC "
+cursor = con.cursor(buffered=True)
+cursor.execute(query)
+
+msgTam = []
+roundTime = []
+
+for (mensagemTxt, roundTripTime) in cursor:
+
+    msgTam.append(len(mensagemTxt))
+    roundTime.append(roundTripTime)
+
+cursor.close()
+
+df=pd.DataFrame({'x': roundTime, 'y': msgTam })
+
+plt.plot( 'x', 'y', data=df, marker='o', color='skyblue')
+plt.show()
+
+
+query = "SELECT numMensagens, tempoGeracao FROM fe02maquinaa.batch order by numMensagens ASC"
+cursor = con.cursor(buffered=True)
+cursor.execute(query)
+
+numM = []
+tGeracao = []
+
+for (numMensagens, tempoGeracao) in cursor:
+
+    numM.append(numMensagens)
+    tGeracao.append(tempoGeracao)
+
+cursor.close()
+
+
+df2=pd.DataFrame({'x': numM, 'y': tGeracao })
+
+plt.plot( 'x', 'y', data=df2, marker='o', color='skyblue')
+plt.show()
