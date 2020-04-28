5582825f12ce82d67f7c3a2a816e28377e5fc8e8
Luís Braga <a82088@alunos.uminho.pt>
2020-04-29 00:37:31 +0100

    avancos

--- TrabalhoPratico/ORCIDinfogetter/orcidinfogetter.py
+++ TrabalhoPratico/ORCIDinfogetter/orcidinfogetter.py
@@ -1,25 +1,81 @@
 import requests
-import json
-import textwrap
+from dbConnection import userscol, pubscol
+
 
 def get_info_publicacao(ORCID_ID_):
-    resp = requests.get("https://pub.orcid.org/v3.0/"+ORCID_ID_+"/works", headers={'Accept': 'application/json'})
+    resp = requests.get("https://pub.orcid.org/v3.0/" + ORCID_ID_ + "/works", headers={'Accept': 'application/json'})
     results = resp.json()
-    my_list = []
+
+    contador = 0
     for r in results['group']:
-        for r2 in r['work-summary']:
+
+        title = r['work-summary'][0]['title']['title']['value']
+
+        doi = 'None'
+
+        for extid in r['external-ids']['external-id']:
+            if extid['external-id-type'] == 'doi':
+                    doi = extid['external-id-value']
+
+        print(doi, ' ', title)
+        contador += 1
+        """for r2 in r['work-summary']:
             title = r2['title']['title']['value']
+            eid = 'None'
+            doi = 'None'
+            wos = 'None'
+            local_de_publicacao = 'None'
+
+            # eid
             try:
                 orcid = r2['url']['value']
                 if orcid.find('eid=') > 0:
                     k1 = orcid.find('eid=')
                     k2 = orcid.find('&', k1)
                     if orcid[k1 + 11:k2] not in my_list:
-                        my_list.append((title,orcid[k1 + 11:k2]))
+                        eid = orcid[k1 + 11:k2]
             except:
-                my_list.append((title, 'None'))
+                eid = 'None'
 
-    print(my_list)
+            # ano
+            try:
+                ano = r2['publication-date']['year']['value']
+            except:
+                ano = 'None'
+
+            # doi
+            try:
+                for extid in r2['external-ids']['external-id']:
+                    if extid['external-id-type'] == 'doi':
+                        doi = extid['external-id-value']
+            except:
+                doi = 'None'
+
+            # wos
+            try:
+                for extid in r2['external-ids']['external-id']:
+                    if extid['external-id-type'] == 'wosuid':
+                        wos = extid['external-id-value']
+            except:
+                wos = 'None'
+
+            # local_de_publicacao
+            try:
+                local_de_publicacao = r2['journal-title']['value']
+            except:
+                local_de_publicacao = 'None'
+
+             my_list.append((title, eid, ano, doi, wos, local_de_publicacao))
+            try:
+                pubscol.insert_one({"_id": title, "eid": eid, "doi": doi, "wos": wos, "local_de_publicacao": local_de_publicacao,
+                                    "autores": [ORCID_ID_], "numero_citacoes": '', "numero_citacoes_ultimos_3_anos": '', "SJR": ''})
+            except:
+                pubscol.update_one({"_id": title}, {"$push": {"autores": ORCID_ID_}})
+            finally:
+                userscol.update_one({"_id": ORCID_ID_}, {"$push": {"publicacoes": title}})"""
+    print(contador)
+    # print(my_list)
+
 
 def readfile(path):
     f = open(path, "r")
@@ -31,5 +87,12 @@ def readfile(path):
             break
         get_info_publicacao(line)
 
+
+def complete_info():
+    for x in userscol.find({}, {"_id": 1}):
+        get_info_publicacao(x['_id'])
+
+
 if __name__ == '__main__':
-    readfile("../ORCIDscraper/orcids.txt")
+    complete_info()
+    # readfile("../ORCIDscraper/orcids.txt")
--- TrabalhoPratico/ORCIDscraper/orcidscraperAlgoritmi.py
+++ TrabalhoPratico/ORCIDscraper/orcidscraperAlgoritmi.py
@@ -1,8 +1,31 @@
 from selenium import webdriver
+import time
+from dbConnection import userscol
 
 class ORCIDAlgoritmiBot:
     def __init__(self):
-        self.driver = webdriver.Chrome()
+        chromeOptions = webdriver.ChromeOptions()
+        prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'javascript': 2,
+                                                            'plugins': 2, 'popups': 2, 'geolocation': 2,
+                                                            'notifications': 2, 'auto_select_certificate': 2,
+                                                            'fullscreen': 2,
+                                                            'mouselock': 2, 'mixed_script': 2, 'media_stream': 2,
+                                                            'media_stream_mic': 2, 'media_stream_camera': 2,
+                                                            'protocol_handlers': 2,
+                                                            'ppapi_broker': 2, 'automatic_downloads': 2,
+                                                            'midi_sysex': 2,
+                                                            'push_messaging': 2, 'ssl_cert_decisions': 2,
+                                                            'metro_switch_to_desktop': 2,
+                                                            'protected_media_identifier': 2, 'app_banner': 2,
+                                                            'site_engagement': 2,
+                                                            'durable_storage': 2, 'profile.managed_default_content_settings.images': 2}}
+        chromeOptions.add_experimental_option('prefs', prefs)
+        chromeOptions.add_argument("start-maximized")
+        chromeOptions.add_argument("disable-infobars")
+        chromeOptions.add_argument("--disable-extensions")
+        chromeOptions.add_argument("--headless")
+        chromeOptions.add_experimental_option("prefs", prefs)
+        self.driver = webdriver.Chrome(options=chromeOptions)
         self.driver.get("http://algoritmi.uminho.pt/members/")
         self.links = []
         self.orcids = []
@@ -21,11 +44,14 @@ class ORCIDAlgoritmiBot:
 
         for link in self.links:
             self.driver.get(str(link))
+            name = self.driver.find_element_by_class_name('Profile-ResearcherName').text
 
             try:
-                self.orcids.append(self.driver.find_element_by_xpath("//a[contains(@href,'http://orcid.org/')]").text)
+                id = self.driver.find_element_by_xpath("//a[contains(@href,'http://orcid.org/')]").text
+                userscol.insert_one({"_id": id, "nome": name, "publicacoes": []})
             except:
-                print(self.driver.find_element_by_class_name('Profile-ResearcherName'). text, ' não possui orcid')
+                print(name, ' não possui orcid')
+
 
     def writetofile(self):
         f = open("orcids.txt", "a")
@@ -38,8 +64,11 @@ class ORCIDAlgoritmiBot:
         bot.getLinks()
         bot.getOrcid()
         self.driver.close()
-        self.writetofile()
+
 
 if __name__ == "__main__":
+    start = time.time()
     my_bot = ORCIDAlgoritmiBot()
+    end = time.time()
+    print("tempo demorado ", end - start)
     my_bot.main(my_bot)
