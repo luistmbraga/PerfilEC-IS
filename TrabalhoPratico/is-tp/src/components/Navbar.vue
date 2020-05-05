9148c5cc2511fd145ba45be795dfb3e85d8e342c
MelhorQueSVN <36746608+MelhorQueSVN@users.noreply.github.com>
2020-05-05 16:05:45 +0100

    minor fix interface

--- TrabalhoPratico/is-tp/src/components/Navbar.vue
+++ TrabalhoPratico/is-tp/src/components/Navbar.vue
@@ -2,8 +2,10 @@
   <nav>
     <v-toolbar dark color="grey lighten-3">
       <v-toolbar-title class="black--text">
+      <div @click="goHome()"> 
         <span>Orcid</span>
         <span class="font-weight-light">Tracker</span>
+      </div>
       </v-toolbar-title>
     </v-toolbar>
   </nav>
@@ -18,6 +20,11 @@ export default {
     return {};
   }, 
   methods:{ 
+    goHome(){ 
+      this.$router.push({
+            name: 'Home',  
+        });
+    }
   } 
 };
 </script>
--- TrabalhoPratico/is-tp/src/views/Home.vue
+++ TrabalhoPratico/is-tp/src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="home">
-    <div>
-      <v-img :src="require('../assets/LogoRealv3.png')" contain aspect-ratio="20"></v-img>
-    </div> 
-    <div style="width:70%;margin-left:auto;margin-right:auto;">
-      <TabelaUsers />
-    </div>  
+    
+    <v-content>
+      <v-container fluid>
+        <v-row align="start" justify="center">
+          <v-col cols="6" sm="18" md="12">
+            <div>
+              <v-img :src="require('../assets/LogoRealv3.png')" contain aspect-ratio="20"></v-img>
+            </div> 
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-content>
+      <v-container fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="12" md="8">
+            <div>
+              <TabelaUsers />
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
   </div>
 </template>
 
